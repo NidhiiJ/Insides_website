ababfa4d825a295e8b51bf7ac014d075ec826281
Nidhi Jain <nidhijain5f2005@gmail.com>
2024-03-21 20:13:17 +0530

    changed whyus card

--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -1,59 +1,47 @@
-import { JSX } from "react/jsx-runtime";
 import Whyustextcard from "./ReusableComponents/Whyustextcard"
 
-interface HeadingInfo {
-    url: string;
-    text: string;
-}
-interface Content {
-    [key: string]: HeadingInfo;
+
+interface ContentProps {
+    heading: string,
+    url: string,
+    text: string
 }
 const WhyUs = () => {
-    const content:Content ={
-        heading1: {
-            'url':'/whyus-1.png',
-            'text': 'We manage your property from A to Z: Tenant search, reception, maintenance.'
+    const content:ContentProps[]=[
+        {   heading:'heading1',
+            url:'/whyus-1.png',
+            text: 'We manage your property from A to Z: Tenant search, reception, maintenance.'
         },
-        heading2: {
+        {
+            heading:'heading2',
             url:'/whyus-2.png',
             text: 'Each month, we guarantee your rent, whatever the occupancy rate.'
         },
-        heading3: {
+        { heading:'heading3',
             url:'/whyus-3.png',
             text: 'You can recover your property whenever you want, the lease is concluded.'
         },
-        heading4: {
-            'url':'/whyus-1.png',
-            'text': 'We manage your property from A to Z: Tenant search, reception, maintenance.'
+         { heading:'heading4',
+            url:'/whyus-1.png',
+            text: 'We manage your property from A to Z: Tenant search, reception, maintenance.'
         },
-        heading5: {
+         { heading:'heading5',
             url:'/whyus-2.png',
             text: 'Each month, we guarantee your rent, whatever the occupancy rate.'
         },
-        heading6: {
+         { heading:'heading6',
             url:'/whyus-3.png',
             text: 'You can recover your property whenever you want, the lease is concluded.'
         },
-    }
+    ]
     
-    const renderTextCard = (content: { [key: string]: HeadingInfo }) => {
-        const textCards: JSX.Element[] = [];
-      
-        for (const header in content) {
-          if (content.hasOwnProperty(header)) {
-            const info = content[header];
-            textCards.push(<Whyustextcard key={header} imgurl={info.url} text={info.text} heading={header} />);
-          }
-        }
-      
-        return textCards;
-      }
-
   return (
     <div id='why-us' className="px-8">
         <div className="text-center text-[32px] md:text-[43px] lg:text-[54px] font-Maharlika pb-7 md:pb-14">Why Choose Us?</div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-10 justify-items-center">
-        {renderTextCard(content)}
+            {content.map((x,i)=>(
+                <Whyustextcard key={i} imgurl={x.url} text={x.text} heading={x.heading} />
+            ))}
         </div>
     </div>
   )
