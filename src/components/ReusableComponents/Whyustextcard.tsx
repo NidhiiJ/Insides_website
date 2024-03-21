557c97b39dd5014cde39e3b2b4fe98628893c9a0
Nidhi Jain <nidhijain5f2005@gmail.com>
2024-03-21 20:18:58 +0530

    whyUs changed

--- src/components/ReusableComponents/Whyustextcard.tsx
+++ src/components/ReusableComponents/Whyustextcard.tsx
@@ -7,8 +7,8 @@ interface CustomProps{
 const Whyustextcard = (props:CustomProps) => {
   return (
     <div className="grid grid-cols-1 gap-2 md:pb-10">
-        <img src={props.imgurl} alt="cant load" className=" md:pb-4 h-full"/>
-        <span className="font-Poppins text-[18px] md:text-[24px] font-semibold">{props.heading}</span>
+        <img src={props.imgurl} alt="cant load" className=" md:pb-4 h-[70%] md:h-full overflow-hidden"/>
+        <div className="font-Poppins text-[18px] md:text-[24px] font-semibold">{props.heading}</div>
         <div className=" max-w-[305px] text-[15px] text-[#AEAEAE]">{props.text}</div>
     </div>
   )
